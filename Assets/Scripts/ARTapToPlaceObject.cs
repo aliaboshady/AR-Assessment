888de328aafadcd50a67435ae88db53242d969b2
Ali Aboshady <ali_aboshady@hotmail.com>
2021-07-19 22:06:06 +0100

    Detects planes and places indicator

--- Assets/Scripts/ARTapToPlaceObject.cs
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -1,31 +1,49 @@
 ﻿using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.Experimental.XR;
 using UnityEngine.XR.ARSubsystems;
 using System.Collections.Generic;
 
 public class ARTapToPlaceObject : MonoBehaviour
 {
-    ARSessionOrigin arOrigin;
+	[SerializeField] GameObject placementIndicator;
 	ARRaycastManager raycastManager;
-	Pose placementObject;
+	Pose placementPose;
+	bool placementPoseIsValid = false;
 
 	private void Start()
 	{
-		arOrigin = FindObjectOfType<ARSessionOrigin>();
 		raycastManager = FindObjectOfType<ARRaycastManager>();
 	}
 
 	private void Update()
 	{
 		UpdatePlacementPose();
+		UpdatePlacementIndicator();
 	}
 
 	private void UpdatePlacementPose()
 	{
 		Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
 		var hits = new List<ARRaycastHit>();
-
 		raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+		placementPoseIsValid = hits.Count > 0;
+		if (placementPoseIsValid)
+		{
+			placementPose = hits[0].pose;
+		}
+	}
+
+	private void UpdatePlacementIndicator()
+	{
+		if (placementPoseIsValid)
+		{
+			placementIndicator.SetActive(true);
+			placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+		}
+		else
+		{
+			placementIndicator.SetActive(false);
+		}
 	}
 }
