d8263b12e280ea70a0707495370fc84df2558f6d
Ali Aboshady <ali_aboshady@hotmail.com>
2021-07-19 18:25:11 +0100

    Testing raycast

---
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    ARSessionOrigin arOrigin;
+	ARRaycastManager raycastManager;
+	Pose placementObject;
+
+	private void Start()
+	{
+		arOrigin = FindObjectOfType<ARSessionOrigin>();
+		raycastManager = FindObjectOfType<ARRaycastManager>();
+	}
+
+	private void Update()
+	{
+		UpdatePlacementPose();
+	}
+
+	private void UpdatePlacementPose()
+	{
+		Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+		var hits = new List<ARRaycastHit>();
+
+		raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+	}
+}
